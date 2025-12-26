4ffb136dd4c0d0aa5ed5d7c52efede24c52f7761
IDilettant <dedenev.dev@gmail.com>
2025-12-26 15:32:26 +0100

    feat(diff): implement flat compare for parsed nodes

---
+++ internal/diff/diff.go
@@ -0,0 +1,90 @@
+package diff
+
+import (
+	"reflect"
+	"sort"
+
+	"code/internal/parser"
+)
+
+func Compare(leftNode, rightNode parser.Node) []Change {
+	sortedKeys := getSortedUnionKeys(leftNode, rightNode)
+
+	changes := make([]Change, 0, len(sortedKeys))
+	for _, key := range sortedKeys {
+		change := buildChangeForKey(key, leftNode, rightNode)
+		changes = append(changes, change)
+	}
+
+	return changes
+}
+
+func getSortedUnionKeys(leftNode, rightNode parser.Node) []string {
+	uniqueKeys := make(map[string]struct{}, len(leftNode)+len(rightNode))
+
+	for key := range leftNode {
+		uniqueKeys[key] = struct{}{}
+	}
+
+	for key := range rightNode {
+		uniqueKeys[key] = struct{}{}
+	}
+
+	keys := make([]string, 0, len(uniqueKeys))
+
+	for key := range uniqueKeys {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	return keys
+}
+
+func buildChangeForKey(key string, leftNode, rightNode parser.Node) Change {
+	leftValue, leftExists := leftNode[key]
+	rightValue, rightExists := rightNode[key]
+
+	switch {
+	case hasOnlyInLeft(leftExists, rightExists):
+		return Change{
+			Key:      key,
+			Type:     Removed,
+			OldValue: leftValue,
+		}
+
+	case hasOnlyInRight(leftExists, rightExists):
+		return Change{
+			Key:      key,
+			Type:     Added,
+			NewValue: rightValue,
+		}
+
+	case isEqualValues(leftValue, rightValue):
+		return Change{
+			Key:      key,
+			Type:     Unchanged,
+			OldValue: leftValue,
+		}
+
+	default:
+		return Change{
+			Key:      key,
+			Type:     Updated,
+			OldValue: leftValue,
+			NewValue: rightValue,
+		}
+	}
+}
+
+func hasOnlyInLeft(leftExists, rightExists bool) bool {
+	return leftExists && !rightExists
+}
+
+func hasOnlyInRight(leftExists, rightExists bool) bool {
+	return !leftExists && rightExists
+}
+
+func isEqualValues(leftValue, rightValue any) bool {
+	return reflect.DeepEqual(leftValue, rightValue)
+}
