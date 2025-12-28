0948026de8eac40782ab1c775a67656270303d23
IDilettant <dedenev.dev@gmail.com>
2025-12-28 22:20:08 +0100

    refactor(app): introduce typed usage/runtime errors and exit codes

---
+++ internal/app/errors.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	exitCodeUsageError   = 2
+	exitCodeRuntimeError = 1
+)
+
+var (
+	ErrUsage   = errors.New("usage error")
+	ErrRuntime = errors.New("runtime error")
+)
+
+func invalidArgsError() error {
+	return fmt.Errorf(
+		"%w: expected %d arguments: <filepath1> <filepath2>",
+		ErrUsage,
+		requiredArgumentsCount,
+	)
+}
+
