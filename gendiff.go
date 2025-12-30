ba0b17e0fa7684ac8a04cc1e7ef54a3f8d104acd
IDilettant <dedenev.dev@gmail.com>
2025-12-30 21:44:36 +0100

    refactor: accept domain.Node in GenDiff

--- gendiff.go
+++ gendiff.go
@@ -2,11 +2,11 @@ package code
 
 import (
 	"code/internal/diff"
+	"code/internal/domain"
 	"code/internal/formatter"
-	"code/internal/parser"
 )
 
-func GenDiff(leftNode, rightNode parser.Node, f formatter.Formatter) string {
+func GenDiff(leftNode, rightNode domain.Node, f formatter.Formatter) string {
 	changes := diff.Compare(leftNode, rightNode)
 
 	return f.Format(changes)
