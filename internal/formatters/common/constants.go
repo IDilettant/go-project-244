5a7b33dc08720fdd67f93df626ec001abe35c168
IDilettant <dedenev.dev@gmail.com>
2026-01-02 18:21:03 +0100

    refactor(formatters): move constants and errors to common package

---
+++ internal/formatters/common/constants.go
@@ -0,0 +1,25 @@
+package common
+
+const (
+	IndentBase         = "  "
+	IndentForUnchanged = "    "
+
+	NewLine = "\n"
+	Space   = " "
+	Dot     = "."
+	Colon   = ":"
+
+	NullString   = "null"
+	ComplexValue = "[complex value]"
+	Quote        = "'"
+
+	OpeningBrace = "{"
+	ClosingBrace = "}"
+	ColonSpace   = ": "
+
+	SignRemoved = "-"
+	SignAdded   = "+"
+
+	FormatStylish = "stylish"
+)
+
--- internal/formatters/common/errors.go
+++ internal/formatters/common/errors.go
@@ -1,4 +1,4 @@
-package formatter
+package common
 
 import (
 	"errors"
