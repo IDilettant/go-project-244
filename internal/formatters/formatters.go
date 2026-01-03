b99ddc02f71f4f082e1edf000f5248949eeeffbb
IDilettant <dedenev.dev@gmail.com>
2026-01-03 12:42:54 +0100

    feat(formatters): support plain formatter selection

--- internal/formatters/formatters.go
+++ internal/formatters/formatters.go
@@ -5,6 +5,7 @@ import (
 
 	"code/internal/diff"
 	"code/internal/formatters/common"
+	"code/internal/formatters/plain"
 	"code/internal/formatters/stylish"
 )
 
@@ -20,6 +21,8 @@ func SelectFormatter(format string) (Formatter, error) {
 	switch format {
 	case emptyFlag, common.FormatStylish:
 		return stylish.New(), nil
+	case common.FormatPlain:
+		return plain.New(), nil
 	default:
 		return nil, fmt.Errorf("%w: %s", common.ErrUnknownFormat, format)
 	}
